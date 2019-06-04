4f33f1eecab162bb3b959a4dbaed280f68986566
Artur <arthurgevorgyan1992@gmail.com>
2019-06-04 20:05:43 +0400

    Add new summary and smart table modals

---
+++ resources/views/tools/bread/modals/summary-table.blade.php
@@ -0,0 +1,43 @@
+<!-- !!! Add form action below -->
+<form role="form" action="{{ route('voyager.bread.filter') }}" method="POST">
+    <div class="modal fade modal-danger modal-relationships" id="summary_table">
+        <div class="modal-dialog relationship-panel">
+            <div class="model-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"
+                            aria-hidden="true">&times;</button>
+                    <h4 class="modal-title"><i class="voyager-heart"></i> {{ str_singular(ucfirst($table)) }}
+                        {{ __('voyager::bread.summary_table') }}
+                    </h4>
+                </div>
+
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-md-12">
+                            {{--@include('voyager::multilingual.language-selector')--}}
+                        </div>
+
+                        <div class="col-md-12 relationship_details">
+                            <p class="relationship_table_select">Table Name</p>
+                            <select class="form-control">
+                                @foreach(\TCG\Voyager\Models\DataType::all() as $val)
+                                    <option value="{{ $val->id }}">{{ $val->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <div class="relationship-btn-container">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">{{ __('voyager::generic.cancel') }}</button>
+                        @if(isset($dataType->id))
+                            <button class="btn btn-danger btn-relationship"><i class="voyager-plus"></i> <span>{{ __('voyager::generic.add') }}</span></button>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <input type="hidden" value="@if(isset($dataType->id)){{ $dataType->id }}@endif" name="data_type_id">
+    {{ csrf_field() }}
+</form>
